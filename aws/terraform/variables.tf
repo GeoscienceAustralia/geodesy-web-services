79b1d25a0a031afd150902fc9e542a769fc57ceb
Frank Fu <frank.fu@ga.gov.au>
2020-05-22 09:15:08 +1000

    Remove cross account access roles

--- aws/terraform/variables.tf
+++ aws/terraform/variables.tf
@@ -3,9 +3,3 @@ variable "region" {}
 variable "environment" {
   description = "Deployment environment."
 }
-
-variable "assumable_by_account_ids" {
-  type        = list(string)
-  description = "A list of IDs of AWS accounts (eg, egeodesy-dev) that are allowed to assume this role."
-  default     = ["094928090547"]
-}
