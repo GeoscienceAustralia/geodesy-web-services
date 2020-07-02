555ca80daabb6a920121954a70fb318f9511e66d
Frank Fu <Frank.Fu@ga.gov.au>
2020-07-02 11:27:54 +1000

    Fix issues based on review

--- gws-core/src/main/java/au/gov/ga/geodesy/domain/model/sitelog/DocumentRepository.java
+++ gws-core/src/main/java/au/gov/ga/geodesy/domain/model/sitelog/DocumentRepository.java
@@ -3,21 +3,15 @@ package au.gov.ga.geodesy.domain.model.sitelog;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 public interface DocumentRepository extends
-        DocumentRepositoryCustom,
         JpaRepository<Document, Integer>,
         QueryDslPredicateExecutor<Document> {
 
     @Override
     List<Document> findAll();
 
-    @Query("select d from Document d where d.id = :id")
-    Document findById(@Param("id") Integer id);
-
-    @Query("select d from Document d where d.name = :name")
     Document findByName(@Param("name") String name);
 }
--- gws-core/src/main/java/au/gov/ga/geodesy/domain/model/sitelog/DocumentRepositoryCustom.java
+++
@@ -1,6 +0,0 @@
-package au.gov.ga.geodesy.domain.model.sitelog;
-
-public interface DocumentRepositoryCustom {
-    void delete(Integer id);
-    void delete(String name);
-}
